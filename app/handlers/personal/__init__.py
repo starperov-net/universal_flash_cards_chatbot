a3bd2b814a8b41019f8387a50a78db8d2cf2f301
Andrey Romanskyy <andrey.romanskyy@icloud.com>
2023-01-05 20:43:56 +0200

    {50} Initial commit
    Signed-off-by: Andrey Romanskyy <andrey.romanskyy@icloud.com>

--- app/handlers/personal/__init__.py
+++ app/handlers/personal/__init__.py
@@ -2,10 +2,16 @@ from aiogram import Dispatcher
 
 # тут додавати импорт нових регістраторов
 from .start import register_handler_start
+from .study import register_handler_study
+# from .start import register_handler_start
 
 
 def register_handlers_personal(dp: Dispatcher) -> None:
     # тут теж треба додати цей регістратор
-    handlers: set = (register_handler_start,)
+    handlers = (
+        register_handler_start,
+        register_handler_study,
+        # register_handler_start,
+        )
     for handler in handlers:
         handler(dp)
