c44bd70d3b3673d703eff64e7ad1f3e125d23725
Kostiantyn Zivenko <kos.zivenko@gmail.com>
2023-01-06 09:59:04 +0100

    added: first paer of query, intermedia commit

--- app/base_functions/learning_sets.py
+++ app/base_functions/learning_sets.py
@@ -18,8 +18,18 @@ async def get_actual_learning_set(
     """
     if not authors:
         authors = [user_id]
-    res = await Card.raw(f"""
-        SELECT * 
-        FROM card
-        WHERE card_id = {user_id}
-    """)
+    res = await Card.raw("""
+        select *
+        from card
+        where (
+        (memorization_stage = 0 AND (card.last_date + interval '5 second') < now()) OR
+        (memorization_stage = 1 AND (card.last_date + interval '25 second') < now()) OR
+        (memorization_stage = 2 AND (card.last_date + interval '120 second') < now()) OR
+        (memorization_stage = 3 AND (card.last_date + interval '600 second') < now()) OR
+        (memorization_stage = 4 AND (card.last_date + interval '3600 second') < now()) OR
+        (memorization_stage = 5 AND (card.last_date + interval '18000 second') < now()) OR
+        (memorization_stage = 6 AND (card.last_date + interval '84600 second') < now())
+        ) AND card.user = {}
+        ORDER BY memorization_stage, card.last_date;
+    """, f"{user_id}")
+    
