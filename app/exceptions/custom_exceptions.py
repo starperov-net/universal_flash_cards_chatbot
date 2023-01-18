da25f757ddb423faafee600a4e65d3d697f5277d
yana <begemotvot2@gmail.com>
2023-01-18 11:37:25 +0200

    {#50} add path to docstring in app.exceptions.custom_exceptions.NotFullSetException
    Signed-off-by: yana <begemotvot2@gmail.com>

--- app/exceptions/custom_exceptions.py
+++ app/exceptions/custom_exceptions.py
@@ -1,6 +1,6 @@
 class NotFullSetException(IndexError):
     """
-    Use inside <check_one_correct_from_four_study_keyboard> func
+    Use inside <app.handlers.personal.keyboards.check_one_correct_from_four_study_keyboard> func
     to check amount of words for learning process
     """
     pass
