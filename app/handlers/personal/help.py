0ec039df7e9c33e4297d8120a76a888d8cda9a5c
Kostiantyn Zivenko <kos.zivenko@gmail.com>
2023-02-13 15:11:47 +0100

    corrected: state name in output

--- app/handlers/personal/help.py
+++ app/handlers/personal/help.py
@@ -5,9 +5,11 @@ from aiogram.fsm.context import FSMContext
 
 
 async def cmd_help(message: types.Message, state: Optional[FSMContext] = None) -> None:
-    await message.answer(
-        f"There will be help. Your state is {state.get_state() if state else 'None'}"
-    )
+    if state:
+        state_name = await state.get_state()
+    else:
+        state_name = "None"
+    await message.answer(f"There will be help. Your state is {state_name}")
 
 
 def register_handler_help(dp: Dispatcher) -> None:
