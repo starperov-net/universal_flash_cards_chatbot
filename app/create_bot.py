41a15fb1a4d012f8b7b1095bf2ad157151d7cd45
Kostiantyn Zivenko <kos.zivenko@gmail.com>
2023-03-07 09:12:39 +0100

    added: redis storage

--- app/create_bot.py
+++ app/create_bot.py
@@ -1,10 +1,16 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 from app.storages import TmpStorage
+from aioredis import Redis
 
 from app.settings import settings
 
-storage = MemoryStorage()
+redis = Redis(host='redis')
+
+
+#storage = MemoryStorage()
+storage = RedisStorage(redis=redis)
 tmp_storage = TmpStorage()
 bot = Bot(token=settings.BOT_TOKEN)  # type: ignore
 dp = Dispatcher(storage=storage, tmp_storage=tmp_storage)
