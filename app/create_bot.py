47620c6f8899471ca00b4fd336396949c4c00fdf
Kostiantyn Zivenko <kos.zivenko@gmail.com>
2023-03-27 09:41:37 +0200

    disabled: Redis storage, connected: MemoryStorage

--- app/create_bot.py
+++ app/create_bot.py
@@ -1,13 +1,15 @@
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.redis import RedisStorage
+#  from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 from app.storages import TmpStorage
-from redis.asyncio import Redis
+#  from redis.asyncio import Redis
 
 from app.settings import settings
 
 
-redis = Redis(host="redis")
-storage = RedisStorage(redis=redis)  # type: ignore
+#  redis = Redis(host="redis")
+#  storage = RedisStorage(redis=redis)  # type: ignore
+storage = MemoryStorage()
 tmp_storage = TmpStorage()
 bot = Bot(token=settings.BOT_TOKEN)  # type: ignore
 dp = Dispatcher(storage=storage, tmp_storage=tmp_storage)
